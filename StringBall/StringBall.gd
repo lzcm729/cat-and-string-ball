746efc494840beafe9417b5e15b819c98469e5ab
lzcm729 <lzcm.729@gmail.com>
2024-06-27 19:01:05 +0800

    线团滚动缩小到一定程度后消失变成红水晶
    线团拥有生命值，体型随着生命值减小而减小，到达阈值后消失，生成红水晶

--- StringBall/StringBall.gd
+++ StringBall/StringBall.gd
@@ -1,49 +1,71 @@
 extends RigidBody2D
-
+var chaos_core = preload("res://ChaosCore/ChaosCore.tscn")
 #@onready var sprite = $Sprite2D
 #@onready var collision = $CollisionShape2D
 
-var size = 100
-var recorded_position
-const single_scaling_factor = Vector2(0.99, 0.99)
+var initial_size = 100.
+var broken_size = 30.
 
-# Called when the node enters the scene tree for the first time.
+@onready var current_size = initial_size
+@onready var recorded_position = position
+@onready var initial_collision_scale = $CollisionShape2D.get_scale()
+@onready var initial_sprite_scale = $Sprite2D.get_scale()
+
+signal show_chaos_core(pos:Vector2)
+
+
 func _ready():
-	apply_central_force((Vector2(10000, 0)))
-	recorded_position = position
+	Hit(Vector2(10000, 0))
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
 func _physics_process(delta):
-	#print(sprite.get_scale())
-	#sprite.apply_scale(sprite.get_scale()*0.9)
-	#collision.apply_scale(collision.get_scale()*0.9)
-	#print(sprite.get_scale())
 	var delta_position = abs(position - recorded_position)
 	recorded_position = position
 	var distance = delta_position.x + delta_position.y
-	print(distance)
-	Scale(MapDistanceToScale(distance))
-	pass
+	MapDistanceToSize(distance)
+	if current_size > broken_size:
+		SetScaleBySize()
+	else:
+		$Sprite2D.hide()
+		show_chaos_core.emit(position)
+		queue_free()
 
-func _on_timer_timeout():
-	#print(collision.get_scale())
-	Scale(single_scaling_factor)
-	#print(collision.get_scale())
 
-func Scale(factor:Vector2):
+# DEPRECATED
+func SelfScale(factor:Vector2):
 	# 碰撞按比例缩小
 	$CollisionShape2D.apply_scale(factor)
 	# sprite需要其他方案
 	$Sprite2D.apply_scale(factor)
-	
+
+
+# DEPRECATED
 func MapDistanceToScale(distance:float) -> Vector2:
+	# 每移动1/10单位距离 缩放*0.99
 	var res = pow(0.99, distance/10)
 	return Vector2(res, res)
+
+
+func MapDistanceToSize(distance:float):
+	# 每移动1/5单位距离 size-1
+	current_size -= distance/5
+
+
+func SetScaleBySize():
+	var ratio = current_size / initial_size
+	$CollisionShape2D.set_scale(initial_collision_scale*ratio)
+	# TODO sprite需要其他方案
+	$Sprite2D.set_scale(initial_sprite_scale*ratio)
+
+
+# 被击打
+func Hit(force:Vector2):
+	apply_central_force(force)
+
 
 # 随着缩小要留下印迹
 func DrawFootprint():
