ba5cd46a6b573bec71d6a5cdd58f4206b167dfdb
ByronZyh <43203171+ByronZyh@users.noreply.github.com>
2024-06-28 03:47:45 +0800

    猫击打球，按下动画才会生成碰撞体

--- cat/cat.gd
+++ cat/cat.gd
@@ -6,8 +6,9 @@ const jump_velocity = -800
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var cat_area = $CatArea  # 猫角色的碰撞区域（Area2D），在场景中命名为 CatArea
+@onready var cat_area = $Marker2D/PatArea  # 猫角色的碰撞区域（Area2D），在场景中命名为 CatArea
 @onready var cat_action = $CatAction
+@onready var animation_tree = $AnimationTree
 
 #记录猫的朝向
 var is_left = false
@@ -18,6 +19,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		is_left = direction == -1
 	
@@ -49,15 +51,17 @@ func _physics_process(delta):
 			cat_action.play('JumpRight')
 		
 	if Input.is_action_just_pressed("ui_patpat"):
-		if is_left:
-			cat_action.play('AttackLeft')
-		else:
-			cat_action.play('AttackRight')
 		#set_collision_layer_value(2, true)
 		for body in cat_area.get_overlapping_bodies():
 			if body.has_method("Hit"):
 				body.Hit(Vector2(10000, 0))  # 调用物体的 Hit 方法，施加力量向量 (10000, 0)
 		#set_collision_layer_value(2, false)
+		if is_left:
+			cat_action.play('AttackLeft')
+			await cat_action.animation_finished
+		else:
+			cat_action.play('AttackRight')
+			await cat_action.animation_finished			
 	
 	move_and_slide()
 	
