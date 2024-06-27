ec90ccd2e25b2273e42c199205bf6b6a3700f3da
lzcm729 <lzcm.729@gmail.com>
2024-06-28 01:57:16 +0800

    被叼起来后隐藏 扔下去重新召唤
    T 叼起来
    G 扔下去

--- StringBall/StringBall.gd
+++ StringBall/StringBall.gd
@@ -1,12 +1,15 @@
 extends RigidBody2D
-var chaos_core = preload("res://ChaosCore/ChaosCore.tscn")
+
+#var chaos_core = preload("res://ChaosCore/ChaosCore.tscn")
 #@onready var sprite = $Sprite2D
 #@onready var collision = $CollisionShape2D
 
-var initial_size = 100.
-var broken_size = 30.
+var initial_hp = 100.
+var broken_ratio = 0.3
 
-@onready var current_size = initial_size
+var is_free_moving = true
+
+@onready var current_hp = initial_hp
 @onready var recorded_position = position
 @onready var initial_collision_scale = $CollisionShape2D.get_scale()
 @onready var initial_sprite_scale = $Sprite2D.get_scale()
@@ -15,7 +18,7 @@ signal show_chaos_core(pos:Vector2)
 
 
 func _ready():
-	#Hit(Vector2(10000, 0))
+	Hit(Vector2(10000, 0))
 	pass
 
 
@@ -24,50 +27,66 @@ func _process(delta):
 
 
 func _physics_process(delta):
-	var delta_position = abs(position - recorded_position)
-	recorded_position = position
-	var distance = delta_position.x + delta_position.y
-	MapDistanceToSize(distance)
-	if current_size > broken_size:
-		SetScaleBySize()
-	else:
-		$Sprite2D.hide()
-		show_chaos_core.emit(position)
-		queue_free()
-
-
-# DEPRECATED
-func SelfScale(factor:Vector2):
-	# 碰撞按比例缩小
-	$CollisionShape2D.apply_scale(factor)
-	# sprite需要其他方案
-	$Sprite2D.apply_scale(factor)
-
-
-# DEPRECATED
-func MapDistanceToScale(distance:float) -> Vector2:
-	# 每移动1/10单位距离 缩放*0.99
-	var res = pow(0.99, distance/10)
-	return Vector2(res, res)
+	if not is_sleeping():
+		var delta_position = abs(position - recorded_position)
+		recorded_position = position
+		var distance = delta_position.x + delta_position.y
+		MapDistanceToSize(distance)
+		if current_hp > initial_hp * broken_ratio:
+			SetScaleBySize()
+		else:
+			$Sprite2D.hide()
+			show_chaos_core.emit(position)
+			queue_free()
 
 
 func MapDistanceToSize(distance:float):
-	# 每移动1/5单位距离 size-1
-	current_size -= distance/5
+	# 每移动1/n单位距离 size-1
+	current_hp -= clampf(distance/50, 0, INF)
 
 
 func SetScaleBySize():
-	var ratio = current_size / initial_size
+	var ratio = current_hp / initial_hp
 	$CollisionShape2D.set_scale(initial_collision_scale*ratio)
 	# TODO sprite需要其他方案
 	$Sprite2D.set_scale(initial_sprite_scale*ratio)
 
+# 随着缩小要留下印迹
+func DrawFootprint():
+	pass
 
+
+# INTERFACE
 # 被击打
 func Hit(force:Vector2):
 	apply_central_force(force)
 
 
-# 随着缩小要留下印迹
-func DrawFootprint():
-	pass
+# INTERFACE
+# 被叼起来 返回此时球相较于初始值的比例
+func BePicked() -> float:
+	set_sleeping(true)
+	hide()
+	return current_hp / initial_hp
+
+
+# INTERFACE
+# 被扔下
+func BeDropped(pos:Vector2):
+	PhysicsServer2D.body_set_state(
+		self.get_rid(),
+		PhysicsServer2D.BODY_STATE_TRANSFORM,
+		Transform2D.IDENTITY.translated(pos)
+	)
+	set_sleeping(false)
+	await get_tree().process_frame
+	show()
+
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_T:
+			BePicked()
+		if event.keycode == KEY_G:
+			BeDropped(Vector2(500, 250))
+
