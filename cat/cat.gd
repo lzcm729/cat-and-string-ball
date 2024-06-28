4bc748cbc8b7c0bfea77135f9e24bff0742fc4cb
ByronZyh <43203171+ByronZyh@users.noreply.github.com>
2024-06-29 02:44:43 +0800

    胜利画面

--- cat/cat.gd
+++ cat/cat.gd
@@ -5,6 +5,7 @@ const speed = 300
 const jump_velocity = -800
 
 signal cat_pat_ball
+signal eat_core
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -141,4 +142,5 @@ func _on_pat_area_body_entered(body):
 		else:
 			body.BeHit(Vector2(10000, 0))
 	if body.name == 'ChaosCore':
+		emit_signal("eat_core")
 		body.BeEaten()
