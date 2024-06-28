260082d435dbb6911e6d762bf42ec68918510634
ByronZyh <43203171+ByronZyh@users.noreply.github.com>
2024-06-29 01:22:48 +0800

    关卡结算

---
+++ GUI/game_over.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var level_id = 1
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_try_again_pressed():
+	get_tree().change_scene_to_file("res://Levels/level_"+str(level_id)+".tscn")
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://GUI/choose_level.tscn")
---
+++ GUI/game_win.gd
@@ -0,0 +1,22 @@
+extends Control
+
+var level_id = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://GUI/choose_level.tscn")
+	
+
+
+
+func _on_next_pressed():
+	get_tree().change_scene_to_file("res://Levels/level_"+str(min(level_id + 1, 5)+".tscn")
