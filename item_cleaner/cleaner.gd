ffc03958a2f2707c56072aefb5c5d2828c42be9c
YiliaLu <VGCOQLU@outlook.com>
2024-06-28 21:25:43 +0800

    cleaner

--- item_cleaner/cleaner.gd
+++ item_cleaner/cleaner.gd
@@ -1,11 +1,26 @@
-extends Node
+extends CharacterBody2D
 
+# 定义移动速度
+var speed = 10000
+# 定义移动方向，初始为向右
+var direction = Vector2(1, 0)
 
-# Called when the node enters the scene tree for the first time.
+# 获取 `cleanerArea`
+@onready var cleaner_area = $CleanerArea
+
 func _ready():
-	pass # Replace with function body.
+	# 连接 `cleanerArea` 的碰撞信号
+	cleaner_area.connect("area_entered", Callable(self, "_on_cleaner_area_entered"))
 
+func _physics_process(delta):
+	# 移动 `cleaner`
+	var velocity = direction * speed
+	move_and_slide()
+	# 检测碰撞，如果碰到边界则反转方向
+	if is_on_wall():
+		direction.x *= -1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_cleaner_area_entered(area):
+	# 如果进入 `cleanerArea` 的是小球，则调用小球的 `hit` 方法
+	if area.name == "Ball":
+		area.hit()
