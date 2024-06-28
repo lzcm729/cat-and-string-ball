4951b9caddf546b096282732614e14794b3b2d12
lzcm729 <lzcm.729@gmail.com>
2024-06-29 00:10:47 +0800

    继续调一下第五关

--- Levels/Stove.gd
+++ Levels/Stove.gd
@@ -10,9 +10,8 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-	if not $FireArea.is_monitoring: return
-	var overlapping_bodies = $FireArea.get_overlapping_bodies()
-	for body in overlapping_bodies:
+	if not $FireArea.is_monitoring(): return
+	for body in $FireArea.get_overlapping_bodies():
 		if body.name == "StringBall":
 			body.BeBurned()
 
