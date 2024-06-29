2a0fa3621157c7c978ef64ebfe41b4fdd056db0f
ByronZyh <43203171+ByronZyh@users.noreply.github.com>
2024-06-29 21:03:45 +0800

    鼠标移动的选关动效

--- GUI/level_entry.gd
+++ GUI/level_entry.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var level_id = 1
-
+@onready var high_light = $highlight
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,3 +15,11 @@ func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			get_tree().change_scene_to_file("res://Levels/level_" + str(level_id) +".tscn")
+
+
+func _on_mouse_entered():
+	high_light.visible = true
+
+
+func _on_mouse_exited():
+	high_light.visible = false
